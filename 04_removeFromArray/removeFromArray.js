779f815cfa29604d07d00b9e4e3abcd663f79bc5
Sammy Hutchinson <hutc0173@algonquinlive.com>
2024-07-29 07:22:02 -0400

    odin war completed challenges

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,11 @@
 const removeFromArray = function (...args) {
   const array = args[0];
   const elementsToRemove = args.slice(1);
+
+  const newArray = array.filter(
+    (element) => !elementsToRemove.includes(element)
+  );
+  return newArray;
 };
 
 // Do not edit below this line
--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,15 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (f) {
+  let temp = ((f - 32) * 5) / 9;
+  return parseFloat(temp.toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (c) {
+  let temp = (c * 9) / 5 + 32;
+  return parseFloat(temp.toFixed(1));
 };
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
